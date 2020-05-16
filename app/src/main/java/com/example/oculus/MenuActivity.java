1e131435affba4788c2d4aa8e1faeb4b455a9b57
dovahkiin21 <saxena.kushagra2112@gmail.com>
2020-05-16 23:58:46 +0530

    second try kushagra

--- app/src/main/java/com/example/oculus/MenuActivity.java
+++ app/src/main/java/com/example/oculus/MenuActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Button;//for another widget
 
 public class MenuActivity extends AppCompatActivity {
 
